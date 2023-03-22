cf63f42a257f61a1731eb8316dc1b32595d459ce
rezwanrahimr <rezwanrahim69@gmail.com>
2023-03-22 18:30:42 +0600

    show data on UI

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import "./Services.css";
 
@@ -11,11 +10,12 @@ const Services = (props) => {
       <Card style={{ width: "16rem" }} className="service-cart">
         <Card.Img variant="top" src={img} />
         <Card.Body>
-          <Card.Title>{name}</Card.Title>
-          <Card.Text>{text}</Card.Text>
-          <p>{age}</p>
-          <p>{time}</p>
-          <Button variant="primary">Go somewhere</Button>
+          <Card.Title className="fw-bolder">{name}</Card.Title>
+          <Card.Text className="text">{text}</Card.Text>
+          <p className="fw-bolder">For Age : {age}</p>
+          <p className="fw-bolder">Time required : {time}s</p>
+
+          <button>Add to list</button>
         </Card.Body>
       </Card>
     </div>
